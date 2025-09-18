24e763e50a54da5c1aec183e642f7499780833eb
Anton Lindeberg <WAYFARER87@users.noreply.github.com>
2025-09-19 04:30:36 +0500

    Improve showtimes UI and button styling

--- lib/features/cinema/models/cinema_showtime.dart
+++ lib/features/cinema/models/cinema_showtime.dart
@@ -2,6 +2,7 @@ class CinemaShowtime {
   const CinemaShowtime({
     required this.time,
     required this.cinemaId,
+    required this.cinemaName,
     required this.room,
     required this.format,
     required this.when,
@@ -11,6 +12,7 @@ class CinemaShowtime {
 
   final String time;
   final String cinemaId;
+  final String cinemaName;
   final String room;
   final String format;
   final String when;
@@ -18,16 +20,27 @@ class CinemaShowtime {
   final String buyUrl;
 
   bool get hasBuyUrl => buyUrl.isNotEmpty;
+  String get cinemaTitle =>
+      cinemaName.isNotEmpty ? cinemaName : cinemaId;
 
   factory CinemaShowtime.fromJson(Map<String, dynamic> json) {
+    final rawCinema = json['cinema'];
+    final rawCinemaId = json['cinema_id'] ?? json['cinemaId'];
+    final rawCinemaName = json['cinema_name'] ??
+        json['cinemaName'] ??
+        json['cinema_title'] ??
+        json['cinemaTitle'];
+
     return CinemaShowtime(
       time: _stringValue(json['time']),
-      cinemaId: _stringValue(json['cinema_id']),
+      cinemaId: _stringValue(rawCinemaId ?? rawCinema ?? rawCinemaName),
+      cinemaName:
+          _stringValue(rawCinemaName ?? rawCinema ?? rawCinemaId),
       room: _stringValue(json['room']),
       format: _stringValue(json['format']),
       when: _stringValue(json['when']),
-      endTime: _stringValue(json['endTime']),
-      buyUrl: _stringValue(json['buyUrl']),
+      endTime: _stringValue(json['endTime'] ?? json['end_time']),
+      buyUrl: _stringValue(json['buyUrl'] ?? json['buy_url']),
     );
   }
 
